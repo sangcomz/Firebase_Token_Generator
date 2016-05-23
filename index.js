923c59a87af5ebacae3516a7ec5266d2885b2544
sangcomz <sangcomz@riiid.co>
2016-05-23 21:30:55 +0900

    fix

--- index.js
+++ index.js
@@ -9,20 +9,6 @@ firebase.initializeApp({
   databaseURL: "https://databaseName.firebaseio.com"
 });
 
-
 var token = firebase.auth().createCustomToken(user);
 
 process.stdout.write("token: " + token);
-
-
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
- 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
- 
-  console.log('앱은 http://%s:%s 에서 작동 중입니다.', host, port);
-});
